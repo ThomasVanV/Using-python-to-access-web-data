9f5a376993740474040adc350ba077d16b07620d
Thomas Van Velkinburgh <thomasvanv@gmail.com>
2018-10-26 02:08:23 -0700

    Add files via upload

---
+++ ex_13_01.py
@@ -0,0 +1,25 @@
+import json
+import urllib.request, urllib.error
+
+url = input("Enter URL: ")
+x = urllib.request.urlopen(url).read()
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)  # this opens the user's url
+data = uh.read()  # read all of the characters in the url
+print('Retrieved', len(data), 'characters')  # print the number of characters
+
+sum = 0
+total_count = 0
+
+info = json.loads(x)
+js_object = info['comments']
+
+
+for item in js_object:
+    num = item.get('count')
+    sum = sum + num
+    total_count = total_count + 1
+
+print('Count: ', total_count)
+print('Sum: ', sum)
